d954a8db80fb379283239d375641c4a2e6584b7b
Li Bai <lbai@temple.edu>
2023-09-10 10:49:15 -0400

    Update app_entrypoint.sh

--- app_entrypoint.sh
+++ app_entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
 
-# setup ros2 environment
-source "/opt/greengrass_bridge/setup.bash"
-source "/opt/ros_demos/setup.bash"
-exec "$@"
+source /opt/ros/humble/setup.bash
+source /opt/greengrass_bridge/setup.bash
+source /opt/ros_demos/setup.bash
+
+printenv
+
+exec "${@:1}"
